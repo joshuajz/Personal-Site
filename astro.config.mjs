732403c1ebbd65e0d35446447c5b29bd705d514e
Josh Cowan <35657686+joshuajz@users.noreply.github.com>
2026-08-31 18:50:21 -0400

    Chore: Astro `6` -> `7`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
-import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+import { rehypeHeadingIds, unified } from "@astrojs/markdown-remark";
 import rehypeFootnoteLinks from "./src/lib/rehype-footnote-links.mjs";
 import rehypeHeadingLinks from "./src/lib/rehype-heading-links.mjs";
 
 export default defineConfig({
   site: "https://joshcowan.com",
+  compressHTML: true,
   markdown: {
-    gfm: true,
-    rehypePlugins: [rehypeHeadingIds, rehypeHeadingLinks, rehypeFootnoteLinks],
+    processor: unified({
+      gfm: true,
+      rehypePlugins: [rehypeHeadingIds, rehypeHeadingLinks, rehypeFootnoteLinks],
+    }),
   },
 });
